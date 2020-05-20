6c2d057c41b4ef04f3a943453a9443f1182e1437
oscar <oscar@mailfake.com>
2020-05-20 22:01:49 +0200

    Eliminación de imports imnecesarios

--- src/main/java/com/mycompany/juegovidarest/api/JuegoVidaController.java
+++ src/main/java/com/mycompany/juegovidarest/api/JuegoVidaController.java
@@ -3,17 +3,11 @@ package com.mycompany.juegovidarest.api;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.mycompany.juegovidarest.dto.Tablero;
 
