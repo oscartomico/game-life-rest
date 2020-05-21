a2ad6731672cc4a83274b569fc9bb461b829df2c
oscar <oscar@mailfake.com>
2020-05-21 16:42:07 +0200

    Modificado el código para añadir controladores

--- src/main/java/com/mycompany/juegovidarest/JuegoVidaRestApplication.java
+++ src/main/java/com/mycompany/juegovidarest/JuegoVidaRestApplication.java
@@ -2,8 +2,10 @@ package com.mycompany.juegovidarest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.mycompany.juegovidarest.api")
 public class JuegoVidaRestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mycompany/juegovidarest/api/JuegoVidaController.java
+++ src/main/java/com/mycompany/juegovidarest/api/JuegoVidaController.java
@@ -1,25 +1,25 @@
 package com.mycompany.juegovidarest.api;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.mycompany.juegovidarest.dto.Tablero;
-
-@Path("/juegovida")
+@CrossOrigin
+@RestController
+@RequestMapping("/juegovida")
 public class JuegoVidaController {
 	@Autowired
 	private JuegoVidaService service;
 	
-	@GET
-	@Path("/itera")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getIteracion(Tablero tablero, @QueryParam("iterations") int iterations) {		
-		return Response.status(200).entity(this.service.itera(tablero.getTablero(), iterations)).build();
-	}	
+//	@RequestMapping(value = "/itera")
+//	public Response getIteracion(Tablero tablero, @QueryParam("iterations") int iterations) {		
+//		return Response.status(200).entity(this.service.itera(tablero.getTablero(), iterations)).build();
+//	}
+	
+	@RequestMapping(value = "/saludo")
+	public ResponseEntity saludo() {		
+		return ResponseEntity.ok().build();
+	}
 }
